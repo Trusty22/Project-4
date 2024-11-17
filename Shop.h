2b068f564f785c5c870ebf7df1247cef3b7a5898
Mandeep Masoun <mandeepmasoun@yahoo.com>
2024-11-16 22:27:59 -0800

    shop done

--- Shop.h
+++ Shop.h
@@ -11,22 +11,23 @@
 using namespace std;
 
 #define kDefaultNumChairs 3
-#define kDeafultNumBarbers 1
+#define kDeafultNumBarbers 1 // added for default barbers and for handeling defined cases.
 
 class Shop {
 public:
+  // changed to include kDeafultNumBarbers and define some conditions
   Shop(int num_barbers, int num_chairs) : max_waiting_cust_((num_chairs > 0) ? num_chairs : kDefaultNumChairs),
                                           cust_drops_(0),
                                           num_barbers_((num_barbers > 0) ? num_barbers : kDeafultNumBarbers) {
     init();
   };
-
+  // chnaged to define some conditions
   Shop() : max_waiting_cust_(kDefaultNumChairs),
            cust_drops_(0), num_barbers_(1) {
     init();
   };
-
-  int visitShop(int customer_id); // return true only when a customer got a service
+  // return when a customer got a service, changed to int to match documention and make passing easier
+  int visitShop(int customer_id);
   void leaveShop(int customer_id, int barber_id);
   void helloCustomer(int id);
   void byeCustomer(int id);
@@ -38,10 +39,16 @@ private:
   int cust_drops_;
   int num_barbers_; // Total number of barbers in the shop
 
-  //
+  // Mutexes and condition variables to coordinate threads
   // mutex_ is used in conjuction with all conditional variables
 
-  struct barber_cond { // Added so condition variables to coordinate threads are held in a struct conditions_struct
+  // I made a structure inspired by others on stack overflow and from the ThreadParam class.
+  // this structer essentially makes it so that the condition variables can interact easily with the threads
+  // It makes it so that each barber has their own threads based on the variables and
+  // This way I dont need to make multiplue classes for a barber or a customer.
+  // The struct will help handle that
+
+  struct barber_cond {
     // allows for each barber to get their own condition variables
     barber_cond() {
       in_service_ = false;
@@ -64,11 +71,13 @@ private:
 
   pthread_cond_t cond_customers_waiting_;
   pthread_mutex_t mutex_;
-  vector<barber_cond *> cond; // vector used to give each barber their own set of condition variables
 
-  int barber = 0; // the id of the barber thread
-  void setBarb(int id) const;
-  void getBarb() const;
+  // This vector is how I keep track of each barbers conditions(wasy handeling for its variables)
+  vector<barber_cond *> cond;
+
+  int barber = 0;             // the id of the barber thread
+  void setBarb(int id) const; // Use set to help with the changing of the current barber number, like a pointer for each threads barber.
+  void getBarb() const;       // easy getting
   void init();
   void print(int person, string message);
   string int2string(int i);
